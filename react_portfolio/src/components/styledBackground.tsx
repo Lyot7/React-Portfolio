945b5d5990b642337e2f88477e6fec48f1667d7f
Ly0t <eliott.bouquerel@gmail.com>
2024-10-04 18:40:27 +0200

    fixed Architecture

---
+++ react_portfolio/src/components/styledBackground.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StyledBackground: React.FC = () => {
+  return (
+    <div className="w-screen h-screen bg-black">
+      <h1 className="">test</h1>
+    </div>
+  );
+};
+
+export default StyledBackground;
--- react_portfolio/src/index.tsx
+++ react_portfolio/src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import StyledBackground from "./components/styledBackground";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode>test</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <StyledBackground />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
